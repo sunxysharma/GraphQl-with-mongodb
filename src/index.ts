96cd4650a42b17244258ca08a5e43acb3f621eff
sunny sharma <sv.sunnysharma@gmail.com>
2020-05-31 17:27:40 +0530

    env

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
+import dotenv from 'dotenv';
 import { ApolloServer, gql } from "apollo-server-express";
 import express from "express";
 import mongoose from "mongoose";
 import { typeDefs } from "./typeDefs";
 import { resolvers } from "./resolvers";
+dotenv.config({ path: ".env" });
 
 const startServer = async () => {
   const app = express();
@@ -12,13 +14,14 @@ const startServer = async () => {
   });
 
   server.applyMiddleware({ app });
-  await mongoose.connect("mongodb://localhost:27017/test", {
+ 
+  await mongoose.connect(process.env.MONGODB_URI || '', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
 
-  app.listen({ port: 4000 }, () =>
-    console.log(`🚀 :happy Server ready at http://localhost:4000${server.graphqlPath}`)
+  app.listen({ port: process.env.PORT }, () =>
+    console.log(`🚀 :happy Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
   );
 };
 
